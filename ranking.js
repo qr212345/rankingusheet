7b166b5d082b1cdb495ec7a9fe73c28e277ff241
abcd1234567890 <asahi114711@icloud.com>
2025-09-23 06:57:42 +0900

    Update ranking.js

--- ranking.js
+++ ranking.js
@@ -3,7 +3,7 @@
 /* =========================
    設定・定数
 ========================= */
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzR9kbVfSCpWxvLMgFUeKfvshIFZGZ_QCRhmBg2InzcwkMvViEWCZ4utg3flIYmDkk/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbzYPvOJR486xArBp10O7dpgJBVLbKACMrDXA9pIB9g93ZGMnxUtJWcqwUgno0tVXcg/exec";
 const ENDPOINT = "https://script.google.com/macros/s/AKfycbzJ0-yF5R7NSvEahxv15ke0AU2lNT8mHSHCLoop74MpUy_-RiFa5Y3OGlq0OBUTr6_t/exec";
 const ADMIN_PASSWORD = "babanuki123";
 const STORAGE_KEY = "rankingPlayerData_v4"; // local cache (but GAS is authoritative)
